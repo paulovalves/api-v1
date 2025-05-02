5141b298dcdc2ee633bae61d05d143580c40b31f
paulovalves <paulovalves@outlook.com>
2025-05-01 23:47:27 -0300

    chore: ajustes nas entidades

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import * as handlebars from 'handlebars';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.useStaticAssets({
+    root: join(__dirname, '..', 'public'),
+    prefix: '/public/',
+  });
+
+  app.setViewEngine({
+    engine: {
+      handlebars: handlebars,
+    },
+    templates: join(__dirname, '..', 'views'),
+  });
 
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     preflightContinue: false,
   });
-
-  app.setGlobalPrefix('api/v1');
 
   const config = new DocumentBuilder()
     .setTitle('API for tests')
@@ -20,7 +39,13 @@ async function bootstrap() {
     .addTag('test')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/v1/api-docs', app, documentFactory);
-  await app.listen(process.env.PORT ?? 8080);
+  SwaggerModule.setup('api/api-docs', app, documentFactory);
+  await app.listen(process.env.PORT ?? 8080, '0.0.0.0');
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`Swagger is running on: ${await app.getUrl()}/api/api-docs`);
+  console.log(`Static assets are served from: ${await app.getUrl()}/public`);
+  console.log(`View engine is set to: ${await app.getUrl()}/views`);
+  console.log(`CORS is enabled for all origins`);
 }
 bootstrap();
